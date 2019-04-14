34dbe084eb5d0b15e5a206c45bb537922cb288f0
jianghua2 <jianghua2@100tal.com>
2019-04-14 14:00:01 +0800

    验证32位操作系统，long型的读写不是原子性的

--- src/main/java/com/thread/chapter1/MutiThreadLong.java
+++ src/main/java/com/thread/chapter1/MutiThreadLong.java
@@ -6,5 +6,45 @@ package com.thread.chapter1;
  * 验证32位操作系统，long型的读写不是原子性的
  */
 public class MutiThreadLong {
+    public static long t = 0;
+
+
+    public static class changT implements Runnable {
+        private long to;
+        public changT(long to) {
+            this.to = to;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                MutiThreadLong.t = to;
+                Thread.yield();
+            }
+        }
+    }
+
+    public static class ReadT implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                long tmp = MutiThreadLong.t;
+                if (tmp != 111L && tmp != -999L && tmp != 333L && tmp != -444L) {
+                    System.out.println(tmp);
+                }
+                Thread.yield();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(new changT(111L)).start();
+        new Thread(new changT(-999L)).start();
+        new Thread(new changT(333L)).start();
+        new Thread(new changT(-444L)).start();
+        new Thread(new ReadT()).start();
+
+    }
+
 
 }
